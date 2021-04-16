b7c324abcdebda5d18f62275b9e7b245f63bf152
Stephen Downey <downeys07@gmail.com>
2021-04-16 16:36:06 -0500

    Adds RefreshScope to TaskerController

--- U2-M8-Challenge-Downey-Stephen/tasker-service/src/main/java/com/trilogyed/tasker/controller/TaskerController.java
+++ U2-M8-Challenge-Downey-Stephen/tasker-service/src/main/java/com/trilogyed/tasker/controller/TaskerController.java
@@ -4,6 +4,7 @@ import com.trilogyed.tasker.exceptions.JdbcOperationFailedException;
 import com.trilogyed.tasker.model.TaskViewModel;
 import com.trilogyed.tasker.service.TaskerServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RefreshScope
 public class TaskerController {
 
     TaskerServiceLayer service;
